037873990ca8777984f1d2a6c6aa58956b959c11
rahul-k76 <145803443+rahul-k76@users.noreply.github.com>
2025-04-13 17:57:30 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,28 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">ONLINE FACULTY FEEDBACK SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Feedback About Teacher
+In this application, students give feedback about the teacher. Students write about teachers they teach good or not. Also, say this teacher is not good for this subject but good for another subject.
+Sometimes we are not able to understand the way of teaching teachers in any one subject, but if the same teacher teaches any other subject, then we understand it very well.
+Most of the students don’t give feedback about the teachers because they are scared if they bad feedback about the teacher, then the teacher cut their marks in the examination.
+In this application, students are given feedback about the teacher, which will be viewed by the Chancellor, Principal, and all faculty members, and if the principal allows then students can also see, but here student ID is visible only to principal and chancellor.
+The student gives positive and negative feedback.
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
